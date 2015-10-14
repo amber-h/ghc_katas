8827240712a8d91294ec28efb5ea28b83702efc1
Molly Bartlett <mollybartlett@MollysMac.local>
2015-10-14 11:47:51 -0500

    Molly - updating more tests

--- java/src/test/java/FibonacciTest.java
+++ java/src/test/java/FibonacciTest.java
@@ -1,18 +1,57 @@
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.Ignore;
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
+@Ignore
 public class FibonacciTest {
 
-	@Ignore
-	@Test
-	public void shouldReturnAnArrayWithTheListOfNumbersInThatSequence() {
-		Fibonacci fibonacci = new Fibonacci();
-		ArrayList<Integer> sequence = fibonacci.forNumber(5);
-		assertEquals(Integer.valueOf(1), sequence.get(0));
-	}
+    @Test
+    public void shouldReturnAnEmptyArrayForZero() {
+        Fibonacci fibonacci = new Fibonacci();
+        ArrayList<Integer> sequence = fibonacci.forNumber(0);
+        assertEquals(0, sequence.size());
+    }
+
+    @Test
+    public void shouldReturnAnArrayOfOne() {
+        Fibonacci fibonacci = new Fibonacci();
+        ArrayList<Integer> sequence = fibonacci.forNumber(1);
+        assertEquals(1, sequence.size());
+        assertEquals(Integer.valueOf(1), sequence.get(0));
+    }
+
+    @Test
+    public void shouldReturnAnArrayWithTheFirst3Numbers() {
+        Fibonacci fibonacci = new Fibonacci();
+        ArrayList<Integer> sequence = fibonacci.forNumber(3);
+        assertEquals(Integer.valueOf(1), sequence.get(0));
+        assertEquals(Integer.valueOf(1), sequence.get(1));
+        assertEquals(Integer.valueOf(2), sequence.get(2));
+    }
+
+    @Test
+    public void shouldReturnAnArrayWithTheFirstFiveNumbers() {
+        Fibonacci fibonacci = new Fibonacci();
+        ArrayList<Integer> sequence = fibonacci.forNumber(5);
+        assertEquals(Integer.valueOf(1), sequence.get(0));
+        assertEquals(Integer.valueOf(1), sequence.get(1));
+        assertEquals(Integer.valueOf(2), sequence.get(2));
+        assertEquals(Integer.valueOf(3), sequence.get(3));
+        assertEquals(Integer.valueOf(5), sequence.get(4));
+    }
+
+    @Test
+    public void shouldReturnAnArrayWithTheListOfTheFirstTenNumbers() {
+        Fibonacci fibonacci = new Fibonacci();
+        ArrayList<Integer> sequence = fibonacci.forNumber(10);
+        assertEquals(10, sequence.size());
+        assertEquals(Integer.valueOf(1), sequence.get(0));
+        assertEquals(Integer.valueOf(1), sequence.get(1));
+        assertEquals(Integer.valueOf(2), sequence.get(2));
+        assertEquals(Integer.valueOf(3), sequence.get(3));
+        assertEquals(Integer.valueOf(5), sequence.get(4));
+    }
 }
--- java/src/test/java/MinesweeperTest.java
+++ java/src/test/java/MinesweeperTest.java
@@ -1,14 +1,64 @@
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Ignore;
+
+import java.util.InputMismatchException;
+
 import static org.junit.Assert.assertEquals;
 
 public class MinesweeperTest {
 
-	@Ignore
-	@Test
-	public void shouldReturnTrueWhenCheckingTwoEmptyArrays() {
-		Minesweeper arrayTester = new Minesweeper("*..");
-		assertEquals("*10", arrayTester.solve());
-	}
+    @Test
+    public void shouldReturnMapWithOneMine() {
+        Minesweeper arrayTester = new Minesweeper("*..");
+        assertEquals("*", arrayTester.solve());
+    }
+
+    @Ignore
+    @Test
+    public void shouldReturnMapWithOneMineAndIndicatingTheAdjacentSpace() {
+        Minesweeper arrayTester = new Minesweeper("*.");
+        assertEquals("*1", arrayTester.solve());
+    }
+
+    @Ignore
+    @Test
+    public void shouldReturnMapWithOneMineAndOneAdjacentAndOneNotNearMines() {
+        Minesweeper arrayTester = new Minesweeper("*..");
+        assertEquals("*10", arrayTester.solve());
+    }
+
+    @Ignore
+    @Test
+    public void shouldReturnMapWithTwoLinesAndOneMine() {
+        Minesweeper arrayTester = new Minesweeper("*..\n...");
+        assertEquals("*10\n110", arrayTester.solve());
+    }
+
+    @Ignore
+    @Test
+    public void shouldReturnMapWithTwoLinesAndMultipleMines() {
+        Minesweeper arrayTester = new Minesweeper("*..\n.*.");
+        assertEquals("*21\n221", arrayTester.solve());
+    }
+
+    @Ignore
+    @Test
+    public void shouldReturnMapWithThreeLines() {
+        Minesweeper arrayTester = new Minesweeper("*..\n..*\n...");
+        assertEquals("*11\n121\n011", arrayTester.solve());
+    }
+
+    @Ignore
+    @Test(expected = InputMismatchException.class)
+    public void shouldThrowAnExceptionForInvalidInput() {
+        new Minesweeper("jibberish");
+    }
+
+    @Ignore
+    @Test
+    public void shouldReturnAMapWithUnevenLines() {
+        Minesweeper arrayTester = new Minesweeper("*..**\n..*\n........*");
+        assertEquals("*12**\n12*\n01221001*", arrayTester.solve());
+    }
 }
