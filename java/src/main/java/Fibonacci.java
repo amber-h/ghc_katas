54057a772b6f98d4931f06c05ed3f70f81ac54ce
nningego <nishitha.ningegowda@gmail.com>
2015-10-13 19:29:02 -0500

    java solutions

--- java/src/main/java/Fibonacci.java
+++ java/src/main/java/Fibonacci.java
@@ -1,12 +1,34 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Fibonacci {
-	public Fibonacci() {
-		
-	}
-	
-	public ArrayList<Integer> forNumber(Integer sequenceMaximum) {
-		return new ArrayList();
-	}
+    public Fibonacci() {
+
+    }
+
+    public ArrayList<Integer> forNumber(Integer sequenceMaximum) {
+        ArrayList<Integer> fibonacci = new ArrayList<>();
+
+        for (int i = 0; i < sequenceMaximum; i++) {
+            fibonacci.add(fibonacciLoop(i+1));
+        }
+
+        return fibonacci;
+
+    }
+
+    public Integer fibonacciLoop(Integer index) {
+        if (index == 1 || index == 2) {
+            return 1;
+        }
+        int firstFibo = 1, secondFibo = 1;
+        int sum = 0;
+        for (int i = 3; i <= index; i++) {
+            //Fibonacci number is sum of previous two Fibonacci number
+            sum = firstFibo + secondFibo;
+            firstFibo = secondFibo;
+            secondFibo = sum;
+
+        }
+        return sum;
+    }
 }
--- java/src/test/java/FibonacciTest.java
+++ java/src/test/java/FibonacciTest.java
@@ -1,18 +1,53 @@
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.Ignore;
-import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
-import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class FibonacciTest {
 
-	@Ignore
-	@Test
-	public void shouldReturnAnArrayWithTheListOfNumbersInThatSequence() {
-		Fibonacci fibonacci = new Fibonacci();
-		ArrayList<Integer> sequence = fibonacci.forNumber(5);
-		assertEquals(Integer.valueOf(1), sequence.get(0));
-	}
+    @Test
+    public void shouldReturn1Forfirst() {
+        Fibonacci fibonacci = new Fibonacci();
+        Integer fibonacciNumber = fibonacci.fibonacciLoop(1);
+
+        assertThat(fibonacciNumber, is(1));
+    }
+
+    @Test
+    public void shouldReturn1ForSecond() {
+        Fibonacci fibonacci = new Fibonacci();
+        Integer fibonacciNumber = fibonacci.fibonacciLoop(2);
+
+        assertThat(fibonacciNumber, is(1));
+    }
+
+    @Test
+    public void shouldReturn2ForThird() {
+        Fibonacci fibonacci = new Fibonacci();
+        Integer fibonacciNumber = fibonacci.fibonacciLoop(3);
+
+        assertThat(fibonacciNumber, is(2));
+    }
+
+    @Test
+    public void shouldReturn610ForFifteenth() {
+        Fibonacci fibonacci = new Fibonacci();
+        Integer fibonacciNumber = fibonacci.fibonacciLoop(15);
+
+        assertThat(fibonacciNumber, is(610));
+    }
+
+    @Test
+    public void should() {
+        Fibonacci fibonacci = new Fibonacci();
+        final ArrayList<Integer> integers = fibonacci.forNumber(15);
+
+        assertThat(integers.size(), is(15));
+        assertThat(integers.get(0), is(1));
+        assertThat(integers.get(14), is(610));
+
+    }
+
 }
