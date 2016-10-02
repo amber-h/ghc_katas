30e15fe7e1a9d010d3f8c5dc6ed0fc10f9dc2619
Amber Houle <amber.houle3@gmail.com>
2016-10-01 22:09:55 -0500

    Adds test cases for pig latin problem

--- java/src/main/java/PigLatin.java
+++ java/src/main/java/PigLatin.java
@@ -1,11 +1,35 @@
-
 /*
+
 https://en.wikipedia.org/wiki/Pig_Latin
+
+For words that begin with consonant sounds, all letters before the initial vowel are placed at the end of the word sequence. Then, "ay" (some people just add "a") is added, as in the following examples:
+
+"pig" → "igpay"
+"banana" → "ananabay"
+"trash" → "ashtray"
+"happy" → "appyhay"
+"duck" → "uckday"
+
+For words that begin with vowel sounds, one just adds "way" to the end. Examples are:
+
+"eat" → "eatway"
+"omelet" → "omeletway"
+"are" → "areway"
+
+For sentences the rules apply to each word individually:
+
+"Hello how are you?" → "Ellohay owhay areway ouyay?"
+
+Treats 'qu' as a single phoneme, ex:
+
+"queen" → "eenquay"
+"square" → "aresquay"
+
  */
 
 public class PigLatin {
 
-	public String translate(String sentence) {
+	public static String translate(String sentence) {
 		return "";
 	}
 }
--- java/src/test/java/PigLatinTest.java
+++ java/src/test/java/PigLatinTest.java
@@ -1,18 +1,59 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-
 public class PigLatinTest {
 
     @Test
     public void convertsASingleWordBeginningWithAVowelToPigLatin(){
-        PigLatin pigLatin = new PigLatin();
-
-        String actualResult = pigLatin.translate("eat");
+        String actualResult = PigLatin.translate("eat");
 
         assertThat(actualResult, is("eatway"));
+    }
+
+    @Test
+    public void convertsASingleWordBeginningWithAConsonantToPigLatin(){
+        String actualResult = PigLatin.translate("cake");
+
+        assertThat(actualResult, is("akecay"));
+    }
+
+
+    @Test
+    public void convertsASingleWordBeginningWithTwoConsonantsToPigLatin(){
+        String actualResult = PigLatin.translate("chocolate");
+
+        assertThat(actualResult, is("ocolatechay"));
+    }
+
+    @Test
+    public void convertsASingleWordBeginningWithThreeConsonantsToPigLatin(){
+        String actualResult = PigLatin.translate("strange");
+
+        assertThat(actualResult, is("angestray"));
+    }
+
+    @Test
+    public void convertsTwoWordsInSentenceToPigLatin(){
+        String actualResult = PigLatin.translate("delicious cheese");
+
+        assertThat(actualResult, is("eliciousday eesechay"));
+    }
+
+    @Test
+    public void convertsWordsBeginningInQuToPigLatin(){
+        String actualResult = PigLatin.translate("queen");
+
+        assertThat(actualResult, is("eenquay"));
+    }
+
+    @Test
+    public void convertsMultipleWordsToPigLating(){
+        String actualResult = PigLatin.translate("I am enjoying the conference");
+
+        assertThat(actualResult, is("Iway amway enjoyingway ethay onferencecay"));
     }
 
 }
