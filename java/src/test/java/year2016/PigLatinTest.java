80dfff81c9efa1745bccb6a3a7698dec44595a6a
Amber Houle <amber.houle3@gmail.com>
2016-10-03 19:02:13 -0500

    Amber - removes unused old build.gradle file, including all tests to be run in gradle test task

--- java/src/test/java/year2016/PigLatinTest.java
+++ java/src/test/java/year2016/PigLatinTest.java
@@ -1,10 +1,9 @@
 package year2016;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class PigLatinTest {
 
